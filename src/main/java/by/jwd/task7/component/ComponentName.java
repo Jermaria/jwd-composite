e89aa8f200d926d4dc517f3a1b104905af39eeaf
Jermaria <53490183+Jermaria@users.noreply.github.com>
2021-01-16 23:20:41 +0300

    Delete src/main directory

--- src/main/java/by/jwd/task7/component/ComponentName.java
+++
@@ -1,5 +0,0 @@
-package by.jwd.task7.component;
-
-public enum ComponentName {
-    CHARACTER, WORD, LEXEME, SENTENCE, PARAGRAPH, TEXT
-}
--- src/main/java/by/jwd/task7/parser/Parser.java
+++
@@ -1,9 +0,0 @@
-package by.jwd.task7.parser;
-
-import by.jwd.task7.component.Component;
-
-public interface Parser {
-    
-    Component parse(String text);
-
-}
